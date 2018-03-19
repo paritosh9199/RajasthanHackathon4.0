2e110503ffee818b71701bf89427b61a919c2efc
Paritosh <paritoshsrivastava9199@gmail.com>
2018-03-19 18:24:55 +0530

    initial beta

---
+++ app/src/main/java/com/hackathon/rajasthon/stockzhackzz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hackathon.rajasthon.stockzhackzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
